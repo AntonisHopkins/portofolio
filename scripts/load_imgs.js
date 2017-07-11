4d320da921dd1d546c4b5029ac51372a2be07fa4
Antonis <tonymis.16@hotmail.com>
2017-07-11 16:25:08 +0300

    Css animations of the portofolio

---
+++ scripts/load_imgs.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+	//classes
+	$wrapper= $(".wrapper");
+	$center= $wrapper.find('.cntr');
+	$leftbar= $wrapper.find('.leftsidebar');
+	$list=$wrapper.find('.list');
+	$home_center = $wrapper.find('.home_center');
+	//id's
+	$home = $list.find('#home');
+	$work = $list.find('#work');
+	//variables
+	var leftbar_data="<h3>Medical Office: <a href='https://github.com/AntonisHopkins/Medical_Office' target='_blank'>GITHUB</a></h3>"
+			+"<div class='slider'>"
+			+"	<ul class='slides'>"
+			+"		<li><img src='./imgs/medical_office/index.jpg' title='Index' class='slide' /></li>"
+			+"		<li><img src='./imgs/medical_office/adminpage.jpg' title='adminpage' class='slide' /></li>"
+			+"		<li><img src='./imgs/medical_office/adminpage2.jpg' title='adminpage2' class='slide' /></li>"
+			+"		<li><img src='./imgs/medical_office/adminpage3.jpg' title='adminpage3' class='slide' /></li>"
+			+"		<li><img src='./imgs/medical_office/check_schedule.jpg' title='check_schedule' class='slide' /></li>"
+			+"	</ul>"
+			+"	<h3>Binary Search Tree:<a href='' target='_blank'>NOT UPLOADED YET</a></h3>"
+			+"	<ul class='slides3'>"
+			+"	<li><img src='./imgs/binary search tree/page_0.jpg' title='page_0' class='slide' /></li>"
+			+"		<li><img src='./imgs/binary search tree/page_1.jpg' title='page_1' class='slide' /></li>"
+			+"		<li><img src='./imgs/binary search tree/page_2.jpg' title='page_2' class='slide' /></li>"
+			+"		<li><img src='./imgs/binary search tree/page_3.jpg' title='page_3' class='slide' /></li>"
+			+"	</ul>"
+			+"	<h3>Cookbook: <a href='https://github.com/AntonisHopkins/Cookbook' target='_blank'>GITHUB</a></h3>"
+			+"	<ul class='slides2'>"
+			+"		<li><img src='./imgs/cookbook/cook-index.jpg' title='Index' class='slide' /></li>"
+			+"		<li><img src='./imgs/cookbook/login.jpg' title='login' class='slide' /></li>"
+			+"		<li><img src='./imgs/cookbook/cook-search.jpg' title='cook-search' class='slide' /></li>"
+			+"		<li><img src='./imgs/cookbook/cook-recipe.jpg' title='recipe' class='slide' /></li>"
+			+"		<li><img src='./imgs/cookbook/adminpage.jpg' title='adminpage' class='slide' /></li>"
+			+"	</ul>"
+			+"</div>"
+	var center_data= "<img src='./imgs/me.jpg' alt='img' title='Me' id='me'/> "
+			+"<h1>Hello Again!</h1><p>Hello again,those are some of my projects!<br/>"
+			+"<u><b>Medical_Office technologies</b></u>: PHP, JAVASCRIPT/JQUERY, AJAX, MYSQL, HTML,CSS.<br/>"
+			+"<u><b>Binary Search Tree technologies</b></u>: JAVASCRIPT/JQUERY, HTML,CSS.<br/>"
+			+"<u><b>Cookbook technologies</b></u>:In this project I did everything but css and html style. I used PHP, MYSQL and some JAVASCRIPT/JQUERY.<br/>"
+			+"<u><b>... This portofolio</b></u>:For this site I did everything, CSS , HTML, and I experiment with JQUERY.</p>";
+	//functions
+	function work_clicked(event){
+		console.log($home_center.is(':visible'));
+		if($home_center.is(':visible')){
+			$home_center.toggle(400);
+			timeOut(600,function(){$center.append(center_data).toggle().fadeIn(400)});
+			timeOut(900,function(){$leftbar.append(leftbar_data).toggle().fadeIn(400);
+				$.getScript("./scripts/slider.js");
+				$.getScript("./scripts/slider2.js");
+				$.getScript("./scripts/slider3.js")});
+		}
+	}
+	function home_clicked(){
+		$center.contents(':not("div")').toggle(400);
+		timeOut(300,function(){$center.contents(':not("div")').remove()});
+		timeOut(600,function(){$center.toggle().fadeIn(400);
+			$home_center.fadeIn(300)});
+		timeOut(800,function(){$leftbar.contents(':not(".leftsidebar")').toggle(400)});
+		timeOut(1200,function(){$leftbar.contents(':not(".leftsidebar")').remove();
+			$leftbar.toggle();
+			timeOut(200,function(){$leftbar.fadeIn(400)});
+		});
+	}
+	function timeOut(time,func){
+		setTimeout(function(){
+			func();
+		},time);
+	}
+	function addPointer($object){
+		$object.each(function(){
+			$(this).find('li').each(function(){$(this).addClass('pointer')
+				$(this).on('click',function(){
+					$(this).addClass('clicked');
+					$(this).removeClass('unclicked');
+					$(this).siblings().addClass('unclicked');
+				});
+			});
+		});
+	}
+	$work.on('click',work_clicked);
+	$home.on('click',home_clicked);
+	addPointer($list);
+});
---
+++ scripts/slider.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+	var animationSpeed=450;
+	var width = 390;
+	var pause=1800;
+	var currentSlide=1;
+	var $slider = $(".slider");
+	var $slideContainer = $slider.find(".slides");
+	var $slides = $slideContainer.find(".slide");
+	var pass=false;
+	var interval;
+	var timeOut;
+	var img;
+	var div;
+	var $popup;
+	var $close;
+	function startSlider(){
+		if(pass){
+			$slideContainer.css('margin-left',0);
+			currentSlide=1;
+		}
+		interval = setInterval(function(){
+			$slideContainer.animate({'margin-left':'-='+width},animationSpeed,function(){
+				currentSlide++;
+				if(currentSlide===$slides.length){
+					timeOut=setTimeout(function(){
+						currentSlide=1;
+						$slideContainer.css('margin-left',0);
+					},700);
+				}
+			});
+			},pause);
+		}
+	function stopSlider(){
+		if(currentSlide===$slides.length){
+			clearInterval(interval);
+			clearTimeout(timeOut);
+			pass=true;
+		}else{
+			clearInterval(interval);
+			clearTimeout(timeOut);
+			pass=false;
+		}
+	}
+	function openSlide(){
+		div = "<div class='popup'></div>";
+		img = $(this).attr("src");
+		img = "<img src='"+ img +"' id='popup_img'></img>";
+		var closeImg = "<img src='./imgs/close.png' id='close'></img>";
+		$(".wrapper").prepend(div);
+		$popup= $(".popup");
+		$popup.append(img);
+		$popup.append(closeImg);
+		$close= $popup.find("#close");
+		$close.css("cursor","pointer");
+		$close.on('click',closeImage);
+	}
+	function closeImage(){
+		$close.remove();
+		$("#popup_img").animate({width: "0%", height: "0%"},100);
+		setTimeout(function()
+		{
+			$popup.remove();
+		},130);
+	}
+	$slides.on('mouseenter',stopSlider).on('mouseleave',startSlider);
+	$slides.on('click',openSlide);
+	$slides.css("cursor","pointer");
+	startSlider();
+});
